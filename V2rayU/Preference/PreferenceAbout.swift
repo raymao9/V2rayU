f00968d3e6d69c6db2b1eb07b8e05dcf092707cd
raymao9 <hellobirdy18@gmail.com>
2019-12-04 23:27:42 +0800

    update version: 1.5.1.0

--- V2rayU/Preference/PreferenceAbout.swift
+++ V2rayU/Preference/PreferenceAbout.swift
@@ -11,7 +11,7 @@ import Preferences
 
 final class PreferenceAboutViewController: NSViewController, PreferencePane {
     let preferencePaneIdentifier = PreferencePane.Identifier.aboutTab
-    let preferencePaneTitle = "關於　"
+    let preferencePaneTitle = "關於"
     let toolbarItemIcon = NSImage(named: NSImage.infoName)!
     
     @IBOutlet weak var VersionLabel: NSTextField!
